bbec0edf8b42924be08dc9df756f2d6bbf28acfc
KhiemGOM <khiem06072007@gmail.com>
2023-04-08 11:31:29 +0700

    Add build files

---
+++ cmake-build-release/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/bishop.cpp.obj"
+  "CMakeFiles/Chess.dir/bishop.cpp.obj.d"
+  "CMakeFiles/Chess.dir/board.cpp.obj"
+  "CMakeFiles/Chess.dir/board.cpp.obj.d"
+  "CMakeFiles/Chess.dir/king.cpp.obj"
+  "CMakeFiles/Chess.dir/king.cpp.obj.d"
+  "CMakeFiles/Chess.dir/knight.cpp.obj"
+  "CMakeFiles/Chess.dir/knight.cpp.obj.d"
+  "CMakeFiles/Chess.dir/main.cpp.obj"
+  "CMakeFiles/Chess.dir/main.cpp.obj.d"
+  "CMakeFiles/Chess.dir/move_result.cpp.obj"
+  "CMakeFiles/Chess.dir/move_result.cpp.obj.d"
+  "CMakeFiles/Chess.dir/pawn.cpp.obj"
+  "CMakeFiles/Chess.dir/pawn.cpp.obj.d"
+  "CMakeFiles/Chess.dir/pieces.cpp.obj"
+  "CMakeFiles/Chess.dir/pieces.cpp.obj.d"
+  "CMakeFiles/Chess.dir/queen.cpp.obj"
+  "CMakeFiles/Chess.dir/queen.cpp.obj.d"
+  "CMakeFiles/Chess.dir/rook.cpp.obj"
+  "CMakeFiles/Chess.dir/rook.cpp.obj.d"
+  "Chess.exe"
+  "Chess.exe.manifest"
+  "Chess.pdb"
+  "libChess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
